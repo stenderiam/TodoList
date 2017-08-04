d7b96e02dd6c3b83bfa199fe5d2595715ff683aa
IlaisStender <kolesnichenko.julie@gmail.com>
2017-08-04 19:20:01 +0300

    TS Test

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import TodoBuilder from './todoBuilder';
+
+import '../css/normalize.css';
+import '../scss/main.scss';
+
+new TodoBuilder();
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.ts',
+    listItem: './src/todolistitem.ts',
+  },
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'TodoList',
+      template: './src/index.html',
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      children: true,
+      async: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
