237f51c8e26b6e281ad357b8f57903ba13b4f8c3
IlaisStender <kolesnichenko.julie@gmail.com>
2017-08-06 01:30:35 +0300

    TS fix

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -92,45 +92,49 @@ var TodoListItem = (function () {
         });
     };
     TodoListItem.prototype.createEntry = function () {
-        this.createElemLi();
-        this.createElemCheckbox();
-        this.createElemDeleteButton();
-        this.createElemInput();
+        this.elemLi = this.createElemLi();
+        this.checkboxElem = this.createElemCheckbox(this.elem);
+        this.deleteButton = this.createElemDeleteButton();
+        this.inputElem = this.createElemInput(this.elem);
         this.elemLi.appendChild(this.checkboxElem);
         this.elemLi.appendChild(this.inputElem);
         this.elemLi.appendChild(this.deleteButton);
         this.todoList.appendChild(this.elemLi);
     };
     TodoListItem.prototype.createElemLi = function () {
-        this.elemLi = document.createElement('li');
-        this.elemLi.className = 'list-item ';
+        var elemLiItem = document.createElement('li');
+        elemLiItem.className = 'list-item ';
+        return elemLiItem;
     };
-    TodoListItem.prototype.createElemCheckbox = function () {
-        this.checkItem = document.createElement('div');
-        this.checkItem.className = 'item-input';
-        this.checkboxElem = document.createElement('input');
-        this.checkboxElem.className = 'item-checkbox';
-        this.checkboxElem.type = 'checkbox';
-        this.checkItem.appendChild(this.checkboxElem);
-        this.checkboxElem.id = "todo" + this.elem.id;
-        this.checkboxElem.checked = this.elem.done;
+    TodoListItem.prototype.createElemCheckbox = function (elem) {
+        var checkItemDiv = document.createElement('div');
+        checkItemDiv.className = 'item-input';
+        var elemCheckboxItem = document.createElement('input');
+        elemCheckboxItem.className = 'item-checkbox';
+        elemCheckboxItem.type = 'checkbox';
+        checkItemDiv.appendChild(elemCheckboxItem);
+        elemCheckboxItem.id = "todo" + elem.id;
+        elemCheckboxItem.checked = elem.done;
+        return elemCheckboxItem;
     };
     TodoListItem.prototype.createElemDeleteButton = function () {
-        this.deleteDiv = document.createElement('div');
-        this.deleteDiv.className = 'item-buttonn';
-        this.deleteButton = document.createElement('button');
-        this.deleteButton.type = 'button';
-        this.deleteButton.className = 'item-delete';
-        this.deleteDiv.appendChild(this.deleteButton);
-        this.deleteButton.innerHTML = "<img src=\"" + deletesvg + "\" alt= \"delete icon\" > ";
+        var elemDeleteDiv = document.createElement('div');
+        elemDeleteDiv.className = 'item-buttonn';
+        var elemDeleteButton = document.createElement('button');
+        elemDeleteButton.type = 'button';
+        elemDeleteButton.className = 'item-delete';
+        elemDeleteButton.innerHTML = "<img src=\"" + deletesvg + "\" alt= \"delete icon\" > ";
+        elemDeleteDiv.appendChild(elemDeleteButton);
+        return elemDeleteButton;
     };
-    TodoListItem.prototype.createElemInput = function () {
-        this.inputItem = document.createElement('div');
-        this.inputItem.className = 'item-text';
-        this.inputElem = document.createElement('input');
-        this.inputElem.className = 'item-input--tag';
-        this.inputElem.type = 'text';
-        this.inputElem.value = this.elem.title;
+    TodoListItem.prototype.createElemInput = function (elem) {
+        var elemInputDiv = document.createElement('div');
+        elemInputDiv.className = 'item-text';
+        var elemInput = document.createElement('input');
+        elemInput.className = 'item-input--tag';
+        elemInput.type = 'text';
+        elemInput.value = elem.title;
+        return elemInput;
     };
     TodoListItem.prototype.deleteItem = function () {
         this.elemLi.remove();
@@ -167,131 +171,6 @@ var TodoListItem = (function () {
     return TodoListItem;
 }());
 exports.default = TodoListItem;
-/*
-let deletesvg = require('../icons/deletesvg.svg');
-
-interface ItodoItemType {
-  title?: string,
-  done?: boolean,
-  id: number
-}
-
-export default class TodoListItem {
-
-  elem: ItodoItemType;
-  todoList: Element;
-  parentContainer: HTMLElement;
-  elemLi: HTMLElement;
-  checkboxElem: HTMLInputElement;
-  deleteButton: HTMLButtonElement;
-  inputElem: HTMLInputElement;
-
-
-  deleteItemEvent: CustomEvent;
-  updateItemEvent: CustomEvent;
-  // detail: any;
-
-  constructor(todoList, todoListContainer, elem) {
-    this.elem = elem;
-    this.todoList = todoList;
-    this.parentContainer = todoListContainer;
-    this.itemCustomEvent();
-    this.createEntry();
-    this.removeItemEvent();
-    this.inputUpdateEvent();
-    this.checkboxUpdateEvent();
-    this.isDone();
-  }
-  itemCustomEvent() {
-    this.deleteItemEvent = new CustomEvent('deleteItem', {
-      detail: { id: this.elem.id },
-    });
-    this.updateItemEvent = new CustomEvent('updateItem', {
-      detail: {},
-    });
-  }
-
-  createEntry() {
-    this.createElemLi();
-    this.createElemCheckbox();
-    this.createElemDeleteButton();
-    this.createElemInput();
-    this.elemLi.appendChild(this.checkboxElem);
-    this.elemLi.appendChild(this.inputElem);
-    this.elemLi.appendChild(this.deleteButton);
-    this.todoList.appendChild(this.elemLi);
-  }
-
-  createElemLi() {
-    // this.elemLi = document.createElement('li');
-    //  this.elemLi.className = 'list-item ';
-    let elemLi: HTMLElement = document.createElement('div');
-    elemLi.className = 'list-item ';
-    this.elemLi = elemLi;
-  }
-  createElemCheckbox() {
-    let checkItem: HTMLElement = document.createElement('div');
-    checkItem.className = 'item-input';
-    let checkboxElem: HTMLInputElement = document.createElement('input');
-    checkboxElem.className = 'item-checkbox';
-    checkboxElem.type = 'checkbox';
-    checkItem.appendChild(checkboxElem);
-    checkboxElem.id = `todo${this.elem.id } `;
-    checkboxElem.checked = this.elem.done;
-    this.checkboxElem = checkboxElem;
-  }
-  createElemDeleteButton() {
-    let deleteDiv: HTMLElement = document.createElement('div');
-    deleteDiv.className = 'item-buttonn';
-    let deleteButton: HTMLButtonElement = document.createElement('button');
-    deleteButton.type = 'button';
-    deleteButton.className = 'item-delete';
-    deleteDiv.appendChild(deleteButton);
-
-    deleteButton.innerHTML = ` < img src= "${deletesvg}" alt= "delete icon" > `;
-    this.deleteButton = deleteButton;
-  }
-  createElemInput() {
-    let inputItem: HTMLElement = document.createElement('div');
-    inputItem.className = 'item-text';
-    let inputElem: HTMLInputElement = document.createElement('input');
-    inputElem.className = 'item-input--tag';
-    inputElem.type = 'text';
-    inputElem.value = this.elem.title;
-    this.inputElem = inputElem;
-  }
-  deleteItem() {
-    this.elemLi.remove();
-  }
-  inputUpdateEvent() {
-    this.inputElem.addEventListener('change', () => {
-      this.updateItemEvent.detail.elem = Object.assign({}, this.elem, { title: this.inputElem.value });
-      this.parentContainer.dispatchEvent(this.updateItemEvent);
-    });
-  }
-  checkboxUpdateEvent() {
-    this.checkboxElem.addEventListener('change', () => {
-      this.updateItemEvent.detail.elem = Object.assign({}, this.elem, { done: this.checkboxElem.checked });
-      this.isDone();
-      this.parentContainer.dispatchEvent(this.updateItemEvent);
-    });
-  }
-
-  isDone() {
-    if (this.checkboxElem.checked) {
-      this.inputElem.classList.add('task-isDone');
-    } else {
-      this.inputElem.classList.remove('task-isDone');
-    }
-  }
-  removeItemEvent() {
-    this.deleteButton.addEventListener('click', () => {
-      this.parentContainer.dispatchEvent(this.deleteItemEvent);
-    });
-  }
-}
-
-*/ 
 
 
 /***/ }),
--- src/todolistitem.ts
+++ src/todolistitem.ts
@@ -8,16 +8,13 @@ export default class TodoListItem {
   todoList: HTMLUListElement;
   parentContainer: HTMLElement;
   elemLi: HTMLLIElement;
-  checkItem: Element;
   checkboxElem: HTMLInputElement;
+  inputElem: HTMLInputElement;
+  deleteButton: HTMLButtonElement;
   deleteItemEvent: CustomEvent;
   updateItemEvent: CustomEvent;
-  deleteDiv: Element;
-  deleteButton: HTMLButtonElement;
-  inputItem: Element;
-  inputElem: HTMLInputElement;
 
-  constructor(todoList, todoListContainer, elem) {
+  constructor(todoList: HTMLUListElement, todoListContainer: HTMLElement, elem: ItodoItemType) {
     this.elem = elem;
     this.todoList = todoList;
     this.parentContainer = todoListContainer;
@@ -38,46 +35,52 @@ export default class TodoListItem {
   }
 
   createEntry() {
-    this.createElemLi();
-    this.createElemCheckbox();
-    this.createElemDeleteButton();
-    this.createElemInput();
+    this.elemLi = this.createElemLi();
+    this.checkboxElem = this.createElemCheckbox(this.elem);
+    this.deleteButton = this.createElemDeleteButton();
+    this.inputElem = this.createElemInput(this.elem);
     this.elemLi.appendChild(this.checkboxElem);
     this.elemLi.appendChild(this.inputElem);
     this.elemLi.appendChild(this.deleteButton);
     this.todoList.appendChild(this.elemLi);
   }
 
-  createElemLi() {
-    this.elemLi = document.createElement('li');
-    this.elemLi.className = 'list-item ';
+  createElemLi(): HTMLLIElement {
+    let elemLiItem = document.createElement('li');
+    elemLiItem.className = 'list-item ';
+    return elemLiItem;
   }
-  createElemCheckbox() {
-    this.checkItem = document.createElement('div');
-    this.checkItem.className = 'item-input';
-    this.checkboxElem = document.createElement('input');
-    this.checkboxElem.className = 'item-checkbox';
-    this.checkboxElem.type = 'checkbox';
-    this.checkItem.appendChild(this.checkboxElem);
-    this.checkboxElem.id = `todo${this.elem.id}`;
-    this.checkboxElem.checked = this.elem.done;
+
+  createElemCheckbox(elem: ItodoItemType): HTMLInputElement {
+    let checkItemDiv: HTMLElement = document.createElement('div');
+    checkItemDiv.className = 'item-input';
+    let elemCheckboxItem = document.createElement('input');
+    elemCheckboxItem.className = 'item-checkbox';
+    elemCheckboxItem.type = 'checkbox';
+    checkItemDiv.appendChild(elemCheckboxItem);
+    elemCheckboxItem.id = `todo${elem.id}`;
+    elemCheckboxItem.checked = elem.done;
+    return elemCheckboxItem;
   }
-  createElemDeleteButton() {
-    this.deleteDiv = document.createElement('div');
-    this.deleteDiv.className = 'item-buttonn';
-    this.deleteButton = document.createElement('button');
-    this.deleteButton.type = 'button';
-    this.deleteButton.className = 'item-delete';
-    this.deleteDiv.appendChild(this.deleteButton);
-    this.deleteButton.innerHTML = `<img src="${deletesvg}" alt= "delete icon" > `;
+
+  createElemDeleteButton(): HTMLButtonElement {
+    let elemDeleteDiv: HTMLElement = document.createElement('div');
+    elemDeleteDiv.className = 'item-buttonn';
+    let elemDeleteButton = document.createElement('button');
+    elemDeleteButton.type = 'button';
+    elemDeleteButton.className = 'item-delete';
+    elemDeleteButton.innerHTML = `<img src="${deletesvg}" alt= "delete icon" > `;
+    elemDeleteDiv.appendChild(elemDeleteButton);
+    return elemDeleteButton;
   }
-  createElemInput() {
-    this.inputItem = document.createElement('div');
-    this.inputItem.className = 'item-text';
-    this.inputElem = document.createElement('input');
-    this.inputElem.className = 'item-input--tag';
-    this.inputElem.type = 'text';
-    this.inputElem.value = this.elem.title;
+  createElemInput(elem: ItodoItemType): HTMLInputElement {
+    let elemInputDiv: HTMLElement = document.createElement('div');
+    elemInputDiv.className = 'item-text';
+    let elemInput = document.createElement('input');
+    elemInput.className = 'item-input--tag';
+    elemInput.type = 'text';
+    elemInput.value = elem.title;
+    return elemInput;
   }
   deleteItem() {
     this.elemLi.remove();
@@ -109,131 +112,3 @@ export default class TodoListItem {
     });
   }
 }
-
-
-
-/*
-let deletesvg = require('../icons/deletesvg.svg');
-
-interface ItodoItemType {
-  title?: string,
-  done?: boolean,
-  id: number
-}
-
-export default class TodoListItem {
-
-  elem: ItodoItemType;
-  todoList: Element;
-  parentContainer: HTMLElement;
-  elemLi: HTMLElement;
-  checkboxElem: HTMLInputElement;
-  deleteButton: HTMLButtonElement;
-  inputElem: HTMLInputElement;
-
-
-  deleteItemEvent: CustomEvent;
-  updateItemEvent: CustomEvent;
-  // detail: any;
-
-  constructor(todoList, todoListContainer, elem) {
-    this.elem = elem;
-    this.todoList = todoList;
-    this.parentContainer = todoListContainer;
-    this.itemCustomEvent();
-    this.createEntry();
-    this.removeItemEvent();
-    this.inputUpdateEvent();
-    this.checkboxUpdateEvent();
-    this.isDone();
-  }
-  itemCustomEvent() {
-    this.deleteItemEvent = new CustomEvent('deleteItem', {
-      detail: { id: this.elem.id },
-    });
-    this.updateItemEvent = new CustomEvent('updateItem', {
-      detail: {},
-    });
-  }
-
-  createEntry() {
-    this.createElemLi();
-    this.createElemCheckbox();
-    this.createElemDeleteButton();
-    this.createElemInput();
-    this.elemLi.appendChild(this.checkboxElem);
-    this.elemLi.appendChild(this.inputElem);
-    this.elemLi.appendChild(this.deleteButton);
-    this.todoList.appendChild(this.elemLi);
-  }
-
-  createElemLi() {
-    // this.elemLi = document.createElement('li');
-    //  this.elemLi.className = 'list-item ';
-    let elemLi: HTMLElement = document.createElement('div');
-    elemLi.className = 'list-item ';
-    this.elemLi = elemLi;
-  }
-  createElemCheckbox() {
-    let checkItem: HTMLElement = document.createElement('div');
-    checkItem.className = 'item-input';
-    let checkboxElem: HTMLInputElement = document.createElement('input');
-    checkboxElem.className = 'item-checkbox';
-    checkboxElem.type = 'checkbox';
-    checkItem.appendChild(checkboxElem);
-    checkboxElem.id = `todo${this.elem.id } `;
-    checkboxElem.checked = this.elem.done;
-    this.checkboxElem = checkboxElem;
-  }
-  createElemDeleteButton() {
-    let deleteDiv: HTMLElement = document.createElement('div');
-    deleteDiv.className = 'item-buttonn';
-    let deleteButton: HTMLButtonElement = document.createElement('button');
-    deleteButton.type = 'button';
-    deleteButton.className = 'item-delete';
-    deleteDiv.appendChild(deleteButton);
-
-    deleteButton.innerHTML = ` < img src= "${deletesvg}" alt= "delete icon" > `;
-    this.deleteButton = deleteButton;
-  }
-  createElemInput() {
-    let inputItem: HTMLElement = document.createElement('div');
-    inputItem.className = 'item-text';
-    let inputElem: HTMLInputElement = document.createElement('input');
-    inputElem.className = 'item-input--tag';
-    inputElem.type = 'text';
-    inputElem.value = this.elem.title;
-    this.inputElem = inputElem;
-  }
-  deleteItem() {
-    this.elemLi.remove();
-  }
-  inputUpdateEvent() {
-    this.inputElem.addEventListener('change', () => {
-      this.updateItemEvent.detail.elem = Object.assign({}, this.elem, { title: this.inputElem.value });
-      this.parentContainer.dispatchEvent(this.updateItemEvent);
-    });
-  }
-  checkboxUpdateEvent() {
-    this.checkboxElem.addEventListener('change', () => {
-      this.updateItemEvent.detail.elem = Object.assign({}, this.elem, { done: this.checkboxElem.checked });
-      this.isDone();
-      this.parentContainer.dispatchEvent(this.updateItemEvent);
-    });
-  }
-
-  isDone() {
-    if (this.checkboxElem.checked) {
-      this.inputElem.classList.add('task-isDone');
-    } else {
-      this.inputElem.classList.remove('task-isDone');
-    }
-  }
-  removeItemEvent() {
-    this.deleteButton.addEventListener('click', () => {
-      this.parentContainer.dispatchEvent(this.deleteItemEvent);
-    });
-  }
-}
-
-*/
